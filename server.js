8e1134535d889d64818a64fd9dc1967a25c124f2
Dodo1213 <36575029+Dodo1213@users.noreply.github.com>
2019-06-08 12:28:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Requires
 const express = require('express');
 const app = express();
+const config = require('./config.json');
 const path = require('path');
 const chalk = require('chalk');
 const morgan = require('morgan');
@@ -9,6 +10,6 @@ app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use(morgan('dev')) // logging
 //Main App Route
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
-const port = 8080;
+const port = config.port;
 //Run Server
 app.listen(process.env.PORT || port, () => console.log(chalk.blue(`Listening intently on port ${port}`)));
